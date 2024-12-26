1b7fb7bb2b177188ea80f210a165046bfdabcf3f
nick_ <nick_@StudyPC>
2024-12-26 09:09:48 +0000

    Added board

---
+++ NGChessApp/src/main/java/com/nick/ngchessapp/NGChessApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.nick.ngchessapp;
+
+/**
+ *
+ * @author nick_
+ */
+public class NGChessApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
